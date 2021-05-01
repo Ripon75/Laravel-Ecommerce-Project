d8b5f9daa5596bfc11c23254cf7179ba167970e4
riponahmed <riponahmedjrtr@gmail.com>
2021-05-01 17:22:01 +0600

    first commit

---
+++ resources/views/frontend/pages/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+  <div class='container margin-top-20'>
+    <h2>Contact Page</h2>
+    <div>
+    <img class="d-block w-25" src="{{ asset('/images/auth/Ripon_Ahmed.jpg') }}" class="admin-image">
+    <h3>Contact number : 01764997485</h3>
+    <h3>Contact email : riponahmedjrtr@gmail.com</h3>
+    </div>
+  </div>
+   
+  </div>
+@endsection
